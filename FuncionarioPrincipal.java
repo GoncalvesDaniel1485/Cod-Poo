8e7a67ffe5cfb98d1d416ac98475469bd269bcf4
Daniel <danielgoncalves1485@gmail.com>
2025-05-07 16:30:48 -0300

    Add files via upload

---
+++ FuncionarioPrincipal.java
@@ -0,0 +1,17 @@
+public class FuncionarioPrincipal{
+    public static void main(String[] args){
+        Funcionario f = new Funcionario(001, "Roberto", "192.147.120-02", "Guarabira", "(83) 98610-0222"
+        , 32, 1512.00);
+    
+        System.out.println("Dados iniciais do funcionário:");
+        System.out.println(f);
+
+        
+        f.setSalario(1850.00);
+
+        
+        System.out.println("\nDados atualizados do funcionário:");
+        System.out.println(f);
+        System.out.printf("Salário líquido: R$ %.2f\n", f.calculaSalarioLiquido());
+    }
+}
