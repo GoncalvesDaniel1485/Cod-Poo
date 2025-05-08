260f2a593c7249e7fa4e2888d203438961c4fe2d
Daniel <danielgoncalves1485@gmail.com>
2025-05-08 19:41:43 -0300

    Update FuncionarioPrincipal.java

--- FuncionarioPrincipal.java
+++ FuncionarioPrincipal.java
@@ -3,14 +3,12 @@ public class FuncionarioPrincipal{
         Funcionario f = new Funcionario(001, "Roberto", "192.147.120-02", "Guarabira", "(83) 98610-0222"
         , 32, 1512.00);
     
-        System.out.println("Dados iniciais do funcionário:");
+        System.out.println("Dados iniciais do funcionário: ");
         System.out.println(f);
-
         
         f.setSalario(1850.00);
-
         
-        System.out.println("\nDados atualizados do funcionário:");
+        System.out.println("\nDados novos do funcionário: ");
         System.out.println(f);
         System.out.printf("Salário líquido: R$ %.2f\n", f.calculaSalarioLiquido());
     }
