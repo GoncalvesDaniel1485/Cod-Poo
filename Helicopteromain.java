1f2abb3bbb657139cf8f5479162f7eb1b9739803
Daniel <danielgoncalves1485@gmail.com>
2025-06-04 18:51:44 -0300

    Update Helicopteromain.java

--- Helicopteromain.java
+++ Helicopteromain.java
@@ -1,29 +1,33 @@
 public class Helicopteromain{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Helicoptero h = new Helicoptero(5);
         Helicoptero e = new Helicoptero(8);
 
-        System.out.println("Helicóptero 1: ");
-        System.out.println("Está ligado? " + h.getLigado());
-        System.out.println("Altitude: " + h.getAltitude());
-        System.out.println("Capacidade: " + h.getCapacidade());
-        System.out.println("Passageiros: " + h.getQuantidade());
-
-        System.out.println("\nHelicóptero 2: ");
-        System.out.println("Está ligado? " + e.getLigado());
-        System.out.println("Altitude: " + e.getAltitude());
-        System.out.println("Capacidade: " + e.getCapacidade());
-        System.out.println("Passageiros: " + e.getQuantidade());
+        System.out.println("Exibir o estado dos helicópteros 1 \n");
+        System.out.println(h + "\n");
+        System.out.println(e + "\n");
         
-   
-
+        System.out.println("\nExibir o estado dos helicópteros 2 \n");
+        h.entra(); h.entra(); h.entra();
         h.ligar();
+        h.decolar(20);
+        System.out.println(h);
+        
+        e.entra();  e.entra(); e.entra(); e.entra(); e.entra(); 
         e.ligar();
+        e.decolar(30);
+        System.out.println(e);
 
-        h.decolar();
-        System.out.println(h.getAltitude());
-        e.decolar();
-        System.out.println(e.getAltitude());
+        System.out.println("\nExibir o estado dos helicópteros 3 \n");
+        h.aterrissar();
+        e.aterrissar();
+        h.desligar();
+        e.desligar();
+        h.sair(); h.sair();
+        e.sair(); e.sair(); e.sair();
+        System.out.println(h);
+        System.out.println(e);
+
     }
     
 }
