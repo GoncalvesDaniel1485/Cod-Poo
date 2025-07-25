cbe8071c8323bf0658fb0d6cc2dc454063bb33f5
Daniel <danielgoncalves1485@gmail.com>
2025-07-25 10:38:10 -0300

    Add files via upload

---
+++ Conta.java
@@ -0,0 +1,66 @@
+public class Conta{
+    private int numero_conta;
+    private String nome_cliente;
+    private double saldo;
+    private boolean ativa;
+
+public Conta(int numero_conta, String nome_cliente){
+        this.numero_conta = numero_conta;
+        this.nome_cliente = nome_cliente;
+    }
+public void depositar(double depositar){
+    if(depositar > 0 && ativa == true){
+        saldo = saldo + depositar;
+        }
+    }
+public void sacar(double sacar){
+    if(sacar <= saldo && ativa == true){
+        saldo = saldo + sacar;
+    }
+}
+
+public void desativar(){
+    if(ativa == true){
+        ativa = false;
+    }
+}
+public void ativar(){
+    if(ativa == false){
+        ativa = true;
+    }
+}
+
+public String exibir(){
+    return "Saldo: " + saldo + "\nNome do usuário: " + nome_cliente + "\nConta de número: " + numero_conta + "\nA conta está ativa? "  + ativa;
+}
+
+public boolean getLigado(){
+    return ligado;
+}
+public double getSaldo(){
+    return saldo;
+}
+public String getNome_cliente(){
+    return nome_cliente;
+}
+public int getNumero_conta(){
+    return numero_conta;
+}
+public void setSaldo(double saldo){
+    this.saldo = saldo;
+}
+public void setNumero_conta(int numero_conta){
+    this.numero_conta = numero_conta;
+}
+public void setNome_cliente(String nome_cliente){
+    this.nome_cliente = nome_cliente;
+}
+
+public void setAtiva(boolean ativa){
+    this.ativa = ativa;
+}
+
+public String toString(){
+    return "Numero da conta: " + numero_conta + "\n Cliente: " + nome_cliente + "\n Saldo:" + saldo + "\n Conta ativa? " + ativa;
+}
+}
---
+++ ContaPrincipal.java
@@ -0,0 +1,41 @@
+public class ContaPrincipal{
+public static void main(String args[]){
+    Conta c1 = new Conta(1, "Roberto");
+    Conta c2 = new Conta(2, "Neto");
+    Conta c3 = new Conta(3, "Ronerto");
+    
+    System.out.println("Exibir estado das contas 1: \n");
+    System.out.println(c1+ "\n");
+    System.out.println(c2+ "\n");
+    System.out.println(c3+ "\n");
+
+    c1.ativar(); 
+    c2.ativar();
+    c3.ativar();
+
+    c1.depositar(500);
+    c2.depositar(600);
+    c3.depositar(8800);
+
+    System.out.println("Exibir estado das contas 2: \n");
+    System.out.println(c1.exibir());
+    System.out.println(c2.exibir());
+    System.out.println(c3.exibir());
+
+    c1.setNome_cliente("AntÔnio");
+    c2.setNome_cliente("Beto");
+    c3.setNome_cliente("Iara");
+
+    System.out.println("Exibir estado das contas 3: \n");
+    System.out.println(c1.exibir());
+    System.out.println(c2.exibir());
+    System.out.println(c3.exibir());
+
+
+
+
+   
+
+
+    }
+}
